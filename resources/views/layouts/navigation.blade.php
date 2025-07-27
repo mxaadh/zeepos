4b2ba7fe297a14e50a56ae695f2b50006a8e1553
Saad Hassan <msaadh909@gmail.com>
2025-07-27 19:19:51 +0500

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Invoice') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="#">
+                        {{ __('Product') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
